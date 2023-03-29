7b60c6c9999cdf9e6987c423556bf5e41a9cf501
BerdiqulovN-dev <navruzbekturalov0333@gmail.com>
2023-03-29 17:32:18 +0500

    0.0.3

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ let users = {};
 
 let elList = document.querySelector(".cards");
 
-fetch("https://fakestoreapi.com/products")
+fetch("https://restcountries.com/v3.1/all")
 	.then((res) => res.json())
 	.then((data) => {
 		users = data;
@@ -11,36 +11,47 @@ fetch("https://fakestoreapi.com/products")
 
 function renderUi(array) {
 	array.forEach((item) => {
-          let elCard=document.createElement("li");
-			console.log(item);
-            elCard.className="card";
-			elCard.innerHTML = `
-                <img src="${item.image}" class="img">
-                <p class="">${item.title}</p>
-                <p class="">price: ${item.price}</p>
-                <p class="">category: ${item.category}</p>
+		let elCard = document.createElement("li");
+		console.log(item);
+		elCard.className = "card";
+		elCard.innerHTML = `
+                <img src="${item.flags.png}" class="img">
+                <p class=""> ${item.name.official}</p>
+                <p class="">population: ${item.population}</p>
+                <p class="">region : ${item.region}</p>
                 </li>
              `;
-        elList.appendChild(elCard);
+		elList.appendChild(elCard);
 	});
 }
 
-let elSelect = document.querySelector(".elSelect");
 let selectArr = [];
 
-users.forEach((item) => {
-	let elOption = document.createElement("option");
-	const arr = item.category;
-	// console.log(arr);
-	arr.forEach((item) => {
-		if (!selectArr.includes(item)) {
-			selectArr.push(item);
-			// console.log(selectArr2);
-			for (let i = 0; i < selectArr.length; i++) {
-				elOption.textContent = selectArr[i];
-				elSelect.append(elOption);
-			}
+// function select(array) {
+// 	array.forEach((item) => {
+// 		arr.forEach((item) => {
+// 			if (!selectArr.includes(item)) {
+// 				selectArr.push(item);
+// 				// console.log(selectArr2);
+// 			}
+// 		});
+// 	});
+// 	}select(users);
+
+	let elSelect = document.querySelector(".form-select");
+
+	elSelect.addEventListener("change", (evt) => {
+		evt.preventDefault();
+		let value = evt.target.value;
+		elList.innerHTML = "";
+		if (value == "all") {
+			renderUi(users);
+			search(allMovies);
+		} else {
+			const newItem = users.filter((item) => {
+				return value == item.region;
+			});
+			renderUi(newItem);
 		}
 	});
-});
 
---
+++ utils.js
@@ -0,0 +1,35 @@
+function renderUi(array) {
+	array.forEach((item) => {
+          let elCard=document.createElement("li");
+			console.log(item);
+            elCard.className="card";
+			elCard.innerHTML = `
+                <img src="${item.image}" class="img">
+                <p class="">${item.title}</p>
+                <p class="">price: ${item.price}</p>
+                <p class="">category: ${item.category}</p>
+                </li>
+             `;
+        elList.appendChild(elCard);
+	});
+}
+
+let elSelect = document.querySelector(".elSelect");
+let selectArr = [];
+
+users.forEach((item) => {
+	let elOption = document.createElement("option");
+	const arr = item.category;
+	// console.log(arr);
+	arr.forEach((item) => {
+		if (!selectArr.includes(item)) {
+			selectArr.push(item);
+			// console.log(selectArr2);
+			for (let i = 0; i < selectArr.length; i++) {
+				elOption.textContent = selectArr[i];
+				elSelect.append(elOption);
+			}
+		}
+	});
+});
+
